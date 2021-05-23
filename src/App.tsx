a4544993bc3c8dc6d8cf06f0535d8dbf93bcc5f8
Ryo Arakawa <67402912+ryo-arakawa@users.noreply.github.com>
2021-05-23 23:25:06 +0900

    intersection Typesについて

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const name = "hello";
+
+let nameChange = "hello";
+
+nameChange = "hello2";
+
+interface NAME {
+  first: string;
+  last: string | null;
+}
+
+// let nameObj: NAME = { first: "Yamada", last: "Taro" };
+let nameObj: NAME = { first: "Yamada", last: "null" };
+
+const func1 = (x: number, y:number) => {
+  return x + y;
+}
+
+// Intersection Types 複数のtypeを結合するもの
+type PROFILE = {
+  age: number;
+  city: string;
+};
+
+type LOGIN = {
+  username: string;
+  password: string;
+};
+
+// この２つの型を結合する
+
+type USER = PROFILE &LOGIN;
+
+const userA: USER = {
+  age:30,
+  city: "Tokyo",
+  username: "xxx",
+  password: "yyy",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
